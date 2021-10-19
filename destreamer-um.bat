e37bf7798fc4850a1b16e3d15f766c79b038976e
thelight0134 <amarhasan0134@gmail.com>
2021-10-19 16:40:00 +0800

    added um script

--- destreamer-um.bat
+++ destreamer-um.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p url=URL: 
 set /p name=Name: 
-destreamer.cmd -i %url% -u kie190043@siswa365.um.edu.my -x -k -t "%name%" --vcodec libx265 --format mp4 -o "E:\Google Drive\UM\1 - DEGREE\5 - SEM 5\destreamer-vid" 
+destreamer.cmd -i %url% -u kie190043@siswa365.um.edu.my -x -k -t "%name%" --vcodec libx265 --format mkv -o "E:\Google Drive\UM\1 - DEGREE\5 - SEM 5\destreamer-vid" 
